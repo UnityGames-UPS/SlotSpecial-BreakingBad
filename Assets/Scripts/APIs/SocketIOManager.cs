141fc4b68e6b48f3fa3fbe448f672e719530f229
maahul-ups <mehul@underpinservices.org>
2026-04-14 15:11:29 +0530

    fix: ver update + b2b OnErr + filename spaces

--- Assets/Scripts/APIs/SocketIOManager.cs
+++ Assets/Scripts/APIs/SocketIOManager.cs
@@ -200,10 +200,21 @@ public class SocketIOManager : MonoBehaviour
   } //Back2 end
   private void OnError(Error err)
   {
-    Debug.LogError("Socket Error Message: " + err);
+    Debug.LogError("[ERROR] Socket error: " + err);
+    if (!string.IsNullOrEmpty(err.message) && err.message.Contains("Session expired"))
+    {
+      Debug.LogWarning("Session expired detected");
+      OnDisconnected();
 #if UNITY_WEBGL && !UNITY_EDITOR
-    JSManager.SendCustomMessage("error");
+        JSManager.SendCustomMessage("session_expired");
 #endif
+    }
+    else
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        JSManager.SendCustomMessage("error");
+#endif
+    }
   }
   private void OnDisconnected() //Back2 Start
   {
